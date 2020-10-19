573e84984241aa434dc6c1342a84ab57380a3f84
AppVeyor <ci@appveyor.com>
2020-10-19 14:46:22 +0000

    7304531

---
+++ precache-manifest.c8a009b0b8bb094b9db49ae8587317a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "042d822d64df81b860884ebb0308b25b",
+    "url": "/R-hw-5-task-2/index.html"
+  },
+  {
+    "revision": "7705d28e2d777dc0b355",
+    "url": "/R-hw-5-task-2/static/css/main.16585c81.chunk.css"
+  },
+  {
+    "revision": "e98a8f8f3b697bdc7094",
+    "url": "/R-hw-5-task-2/static/js/2.5ec0ceef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/R-hw-5-task-2/static/js/2.5ec0ceef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7705d28e2d777dc0b355",
+    "url": "/R-hw-5-task-2/static/js/main.240bf8d7.chunk.js"
+  },
+  {
+    "revision": "2a2c8234ed2090f5f639",
+    "url": "/R-hw-5-task-2/static/js/runtime-main.a1e9418c.js"
+  },
+  {
+    "revision": "616402edefe38ddbbeca60717b15ea36",
+    "url": "/R-hw-5-task-2/static/media/icon.616402ed.svg"
+  },
+  {
+    "revision": "17a428f231c4d1b91b4ea4f60c15d6db",
+    "url": "/R-hw-5-task-2/static/media/image.17a428f2.jpg"
+  }
+]);
